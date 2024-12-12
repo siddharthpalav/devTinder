b241f8c86a84f3e51f46e5c69b0b019ddb911a22
siddhartha <siddhartha@deseo.tech>
2024-12-12 06:02:16 +0530

    adding validations for schema

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,22 +1,33 @@
-const adminAuth = (req, res, next) => {
-  console.log("Admin auth is getting checked!!");
-  const token = "xyz";
-  const isAdminAuthorized = token === "xyzz";
-  if (!isAdminAuthorized) {
-    res.status(404).send("Unauthorized request");
-  } else {
-    next();
-  }
-};
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const userAuth = (req, res, next) => {
-  console.log("User auth is getting checked!!");
-  const token = "xyz";
-  const isAdminAuthorized = token === "xyzz";
-  if (!isAdminAuthorized) {
-    res.status(404).send("Unauthorized request");
-  } else {
+const userAuth = async (req, res, next) => {
+  try {
+    // Read the token from the req cookies
+    const { token } = req.cookies;
+
+    if (!token) {
+      throw new Error("Token is not valid!!!!");
+    }
+
+    // Validate the token
+    const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+
+    const { _id } = decodedObj;
+
+    // Find the user
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // attaching user on the request object;
+    req.user = user;
+
     next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 };
-module.exports = { adminAuth, userAuth };
+module.exports = { userAuth };
